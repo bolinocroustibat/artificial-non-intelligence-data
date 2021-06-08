10991447bf39d29f98114a28dbc808c2063a5020
Adrien Carpentier <moi@adriencarpentier.com>
2021-06-09 00:04:02 +0900

    Add example with connection to the DB

---
+++ README.md
@@ -0,0 +1,5 @@
+# Artificial Non Intelligence - Data Analysis
+
+```sh
+export DATABASE_URL=postgres://{user}:{password}@{hostname}:{port}/{database-name}
+```
--- app.py
+++ app.py
@@ -1,12 +1,31 @@
+import os
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import psycopg2
+import seaborn as sns
 import streamlit as st
 
-import pandas as pd
-import seaborn as sns
 
-st.title("Artificial Intelligence Data Analysis")
+DATABASE_URL = os.environ['DATABASE_URL']
 
-st.write("Test")
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-df = pd.DataFrame({'x': [1, 2, 3], 'y': [10, 30, 70]})
-sns.lineplot(x='x', y='y', data=df)
+st.title("Artificial Non Intelligence Data Analysis")
+
+st.write("Answers from game's users")
+
+connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+with connection:
+    query: str = f"SELECT answer FROM answers;"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    answers = cursor.fetchall()
+    cursor.close()
+
+df = pd.DataFrame(answers)
+
+sns.displot(data=df)
 st.pyplot()
+
+st.write("Test2")
