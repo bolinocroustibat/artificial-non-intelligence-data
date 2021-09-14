8b328a01a68a5ddd60b138bd0d90d99d0ae7d23c
Adrien Carpentier <moi@adriencarpentier.com>
2021-09-15 00:22:47 +0900

    Use MySQL, fix some SQL issues

--- app.py
+++ app.py
@@ -2,23 +2,37 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-import psycopg2
+import pymysql
 import seaborn as sns
 import streamlit as st
 
+from settings import (
+    DATABASE_DB,
+    DATABASE_HOST,
+    DATABASE_PASSWORD,
+    DATABASE_USER,
+    DATABASE_PORT,
+)
 
-DATABASE_URL = os.environ['DATABASE_URL']
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option("deprecation.showPyplotGlobalUse", False)
 st.set_page_config(
-    page_title="Artificial Non Intelligence - Live Data Analysis", page_icon="https://artificial-non-intelligence.herokuapp.com/style/favicon.ico",
-    layout='centered',
-    initial_sidebar_state='auto'
+    page_title="Artificial Non Intelligence - Live Data Analysis",
+    page_icon="https://artificial-non-intelligence.herokuapp.com/style/favicon.ico",
+    layout="centered",
+    initial_sidebar_state="auto",
 )
 
 st.title("Artificial Non Intelligence - Live Data Analysis")
 
-connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+connection = pymysql.connect(
+    host=DATABASE_HOST,
+    user=DATABASE_USER,
+    password=DATABASE_PASSWORD,
+    database=DATABASE_DB,
+    port=DATABASE_PORT,
+)
+
 with connection:
     cursor = connection.cursor()
 
@@ -28,7 +42,7 @@ with connection:
     sessions_count = cursor.fetchone()
 
     # Get the number of questions
-    query: str = "SELECT real, count(*) AS a FROM questions GROUP by real;"
+    query: str = "SELECT `real`, count(*) AS a FROM questions GROUP by `real`;"
     cursor.execute(query)
     comments_count: int = cursor.fetchall()
 
@@ -40,30 +54,19 @@ with connection:
     # Count number of different answers for each question
     query: str = """
     SELECT
-        ca.id,
-        ca.content,
-        ca.real,
-        ca.answer,
+        questions.id,
+        questions.content,
+        questions.real,
+        answers.answer,
         count(*) AS count
-    FROM (
-        SELECT
-            questions.id,
-            questions.content,
-            questions.real,
-            answers.answer
-        FROM
-            questions
-            INNER JOIN answers ON questions.id = answers.question_id
-        GROUP BY
-            questions.id,
-            questions.real,
-            answers,
-            answer) AS ca
+    FROM
+        questions
+        INNER JOIN answers ON questions.id = answers.question_id
     GROUP BY
-        ca.id,
-        ca.content,
-        ca.real,
-        ca.answer;
+        questions.id,
+        questions.content,
+        questions.real,
+        answers.answer;
     """
     cursor.execute(query)
     answers = cursor.fetchall()
@@ -76,10 +79,12 @@ st.markdown("<h2>Number of answers</h2>", unsafe_allow_html=True)
 total_answers_count: int = answers_count[0][1] + answers_count[1][1]
 st.write(total_answers_count)
 
-df = pd.DataFrame(answers, columns =['id', 'content', 'real', 'answer', 'count'])
+df = pd.DataFrame(answers, columns=["id", "content", "real", "answer", "count"])
 
-# # Verify consistency of total number of answers
-# assert df['count'].sum(axis = 0, skipna = True) == total_answers_count
+# Verify consistency of total number of answers
+# st.write(df['count'].sum(axis = 0, skipna = True))
+# st.write(total_answers_count)
+assert df['count'].sum(axis = 0, skipna = True) == total_answers_count
 
 # Count total number of human right guesses
 right_guesses: int = 0
@@ -87,16 +92,10 @@ wrong_guesses: int = 0
 biggest_good_guesses: int = 0
 biggest_wrong_guesses: int = 0
 for i in df.index:
-    if df['real'][i] == df['answer'][i]:
-        right_guesses += df['count'][i]
-        if df['count'][i] > biggest_good_guesses:
-            biggest_good_guesses = df['count'][i]
-            best_comment = (i, df["content"][i], df['count'][i], df['real'][i])
+    if df["real"][i] == df["answer"][i]:
+        right_guesses += df["count"][i]
     else:
-        wrong_guesses += df['count'][i]
-        if df['count'][i] > biggest_wrong_guesses:
-            biggest_wrong_guesses = df['count'][i]
-            worst_comment = (i, df["content"][i], df['count'][i], df['real'][i])
+        wrong_guesses += df["count"][i]
 
 accuracy: float = round(right_guesses / total_answers_count, 4)
 
@@ -106,14 +105,18 @@ st.write(accuracy)
 sns.barplot(x=["Right guesses", "Wrong guesses"], y=[right_guesses, wrong_guesses])
 st.pyplot()
 
+# st.markdown("<h2>Easiest question</h2>", unsafe_allow_html=True)
+# st.write(
+#     f'{best_comment[0]}: "{best_comment[1]}" ({best_comment[2]} right answers, real={best_comment[3]})'
+# )
+
+# st.markdown("<h2>Most difficult question</h2>", unsafe_allow_html=True)
+# st.write(
+#     f'{worst_comment[0]}: "{worst_comment[1]}" ({worst_comment[2]} wrong answers, real={worst_comment[3]})'
+# )
+
 st.markdown("<h2>Best score</h2>", unsafe_allow_html=True)
 st.write(sessions_count[0])
-
-st.markdown("<h2>Easiest question</h2>", unsafe_allow_html=True)
-st.write(f'{best_comment[0]}: "{best_comment[1]}" ({best_comment[2]} right answers, real={best_comment[3]})')
-
-st.markdown("<h2>Most difficult question</h2>", unsafe_allow_html=True)
-st.write(f'{worst_comment[0]}: "{worst_comment[1]}" ({worst_comment[2]} wrong answers, real={worst_comment[3]})')
 
 st.markdown("<h2>Current dataset</h2>", unsafe_allow_html=True)
 total_comments_count: int = comments_count[0][1] + comments_count[1][1]
